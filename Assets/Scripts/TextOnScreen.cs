0007d830383b6ad981c6b7b2724216c7d63c2409
Somersault <Somersault0023@gmail.com>
2025-06-01 23:48:56 +0200

    Final commit

--- Assets/Scripts/TextOnScreen.cs
+++ Assets/Scripts/TextOnScreen.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using System.Collections;
+using TMPro;
 
-public class TextOnScreen : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class TextOnScreen : MonoBehaviour{
+    public TMP_Text text;
+    [SerializeField] private int textIndex;
+    [SerializeField] private bool showInitMessages = true;
+    private string[] texts = {
+        "¡Bienvenido al juego!",
+        "Usa W-A-S-D para moverte.",
+        "Presiona espacio para saltar.",
+        "Y evita a los enemigos peligrosos.",
+        "¡Buena suerte y diviértete!",
+        "¡HAS DESCUBIERTO UN EASTER EGG!",
+        "¡ENHORABUENA! ¡HAS GANADO!",
+    };
+
+    void Start(){
+        if (showInitMessages) { StartCoroutine(ShowWelcomeMessages()); }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator ShowWelcomeMessages(){
+        for (int i = 0; i < texts.Length - 2; i++) {
+            Debug.Log(texts[i]);
+            UpdateText(texts[i]);
+            yield return new WaitForSeconds(2f);
+            text.gameObject.SetActive(false);
+        }
+    }
+    
+    private void UpdateText(string txt){
+        text.text = txt;
+        text.gameObject.SetActive(true);
+    }
+    
+    public void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("PlayerDetection")) { StartCoroutine(ShowText(textIndex, 3f)); }
+    }
+
+    IEnumerator ShowText(int idx, float duration = 3f) {
+        UpdateText(texts[idx]);
+        yield return new WaitForSeconds(duration);
+        text.gameObject.SetActive(false);
     }
 }
